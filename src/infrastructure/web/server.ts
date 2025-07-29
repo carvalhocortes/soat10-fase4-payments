6e68356c0b9cd02a5e2ddf3e27b4707cf45befa0
CarvalhoCortes <carvalhocortes@gmail.com>
2025-07-29 16:47:39 -0300

    fix: adicionar logs para exibir informações da fila SQS e tópico SNS

--- src/infrastructure/web/server.ts
+++ src/infrastructure/web/server.ts
@@ -33,6 +33,16 @@ class Server {
 
       const queueUrl = process.env.SQS_PAYMENT_QUEUE_URL || '';
       const topicArn = process.env.SNS_PAYMENT_TOPIC_ARN || '';
+      console.log('');
+      console.log('');
+      console.log('');
+      console.log('');
+      console.log(`Listening to SQS queue: ${queueUrl}`);
+      console.log(`Publishing to SNS topic: ${topicArn}`);
+      console.log('');
+      console.log('');
+      console.log('');
+      console.log('');
       const sqsListener = createSqsListener(queueUrl, topicArn);
 
       const pollMessages = async () => {
